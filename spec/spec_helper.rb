98e0260ac6c73248f9d21f33cd868f49a996c455
luke-dr <ldressel@gmail.com>
2013-03-13 14:49:54 -0700

    Associate users to tickets when tickets are created

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,6 +9,11 @@ require 'rspec/autorun'
 Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 
 RSpec.configure do |config|
+  config.before do
+    ActionMailer::Base.deliveries.clear
+  end
+
+  config.include EmailSpec::Helpers
   # ## Mock Framework
   #
   # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
@@ -36,5 +41,4 @@ RSpec.configure do |config|
   #     --seed 1234
   config.order = "random"
 
-  config.include EmailSpec::Helpers
 end
--- spec/support/factories/user_factory.rb
+++ spec/support/factories/user_factory.rb
@@ -1,7 +1,20 @@
 FactoryGirl.define do
   factory :user do
-    sequence(:email) { |n| "user#{n}@ticketee" }
+    email "ticketee@email.com"
     password "password"
     password_confirmation "password"
+
+    factory :confirmed_user do
+      after_create do |user|
+        user.confirm!
+      end
+    end
   end
 end
+
+
+    #sequence(:email) { |n| "user#{n}@ticketee.com" }
+    #password "password"
+    #password_confirmation "password"
+  #end
+#end

3b0cf7960ce41731dacd44f704f8c4d78bd4ad1f
luke-dr <ldressel@gmail.com>
2013-03-23 12:53:49 -0700

    Users can tag tickets upon creation

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -2,11 +2,23 @@ class Ticket < ActiveRecord::Base
   belongs_to :project
   belongs_to :state
   belongs_to :user
-  attr_accessible :title, :description, :assets_attributes
+  attr_accessible :title, :description, :assets_attributes, :tag_names
+  attr_accessor :tag_names
   validates :title, :presence => true
   validates :description, :presence => true,
       :length => { :minimum => 10 }
   has_many :assets
   accepts_nested_attributes_for :assets
   has_many :comments
+  has_and_belongs_to_many :tags
+  before_create :associate_tags
+
+  private
+  def associate_tags
+    if tag_names
+      tag_names.split(" ").each do |name|
+        self.tags << Tag.find_or_create_by_name(name)
+      end
+    end
+  end
 end
